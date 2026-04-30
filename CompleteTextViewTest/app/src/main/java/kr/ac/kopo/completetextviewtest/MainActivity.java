76c7bef6091d2f9bf6fdb0bb83fd62628723df34
jaeyoon2250 <jaeyoon2250@gmail.com>
2026-04-30 15:52:51 +0900

    Bars, Scroll 연습

---
+++ CompleteTextViewTest/app/src/main/java/kr/ac/kopo/completetextviewtest/MainActivity.java
@@ -0,0 +1,41 @@
+package kr.ac.kopo.completetextviewtest;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView auto;
+    MultiAutoCompleteTextView multi;
+    String[] items = {"ball", "banana", "base", "balloon", "canada", "carrot", "candy", "card", "door"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auto = findViewById(R.id.auto);
+        multi = findViewById(R.id.multi);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
+
+        auto.setAdapter(adapter);
+
+        MultiAutoCompleteTextView.CommaTokenizer tokenizer = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(tokenizer);
+        multi.setAdapter(adapter);
+    }
+}
